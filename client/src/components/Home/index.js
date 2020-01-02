1bab25476ee779d3e87ac9c7775d80f9ff79a23a
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-01 20:08:46 -0500

    btns now redirect to chat rooms

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'
 import API from '../../utils/API';
-
+import Header from '../Header';
+import { Redirect } from 'react-router-dom';
 const Home = (props) => {
 
   const [rooms, setRooms] = useState([]);
   const [newRoom, setNewRoom] = useState('');
+  const [goChat, setGoChat] = useState(false)
+
+  const testing = true;
 
   useEffect(() => {
     API.getRooms()
@@ -16,6 +20,12 @@ const Home = (props) => {
       })
       .catch(err => console.log(err))
   }, [])
+
+  const redirectChat = () => {
+    if (goChat) {
+      return <Redirect to='/chat' />
+    }
+  }
 
   const deleteRooms = () => {
     API.deleteRooms()
@@ -27,37 +37,41 @@ const Home = (props) => {
     setNewRoom(e.target.value);
   }
 
-  const createRoom = () => {
-
+  const handleRoomBtn = (e) => {
+    console.log(e.target.dataset.room);
+    props.setRoom(e.target.dataset.room);
+    setGoChat(true)
   }
 
   return (
     <div className='home'>
-      <h1>This is the Home Page</h1>
-
-      <div className="test-btn-area">
-        <button onClick={() => { props.setRoom('testing') }}>test room</button>
-        <button onClick={() => { deleteRooms() }}>Delete Rooms</button>
-        <Link to='/chat'>Chat</Link>
-      </div>
-
-      <hr />
+      <Header />
+      {redirectChat()}
       <div className="rooms-container">
         <div className="left-side">
-          <h3>Rooms to play in</h3>
+          <h3 className='text-center'>Open Rooms</h3>
+
           {rooms.map((room, i) => (
-            <button onClick={() => { props.setRoom(room.name) }} key={i}>name: {room.name}</button>
+            <button onClick={(e) => { handleRoomBtn(e) }} key={i} data-room={room.name}>name: {room.name}</button>
           ))}
+
+          {testing ? <div className="test-btn-area">
+            <button onClick={(e) => { handleRoomBtn(e) }} data-room='testing'>test room</button>
+            <button onClick={() => { deleteRooms() }}>Delete Rooms</button>
+            <button onClick={() => { setGoChat(true) }}>Go Chat</button>
+            <Link to='/chat'>Chat</Link>
+          </div> : <></>}
+
         </div>
         <div className="right-side">
-          <h3>Create Room</h3>
+          <h3 className='text-center'>Create Room</h3>
           <div className="create-room-form">
             <div className="input-group">
               <label htmlFor="name">Room Name</label>
               <input type="text" name="name" id="name" value={newRoom} onChange={(e) => { handleInputNewRoom(e) }} />
             </div>
             <div className="input-group">
-              <button className="createBtn" onClick={() => { props.setRoom(newRoom) }}>Create Room</button>
+              <button className="createBtn" data-room={newRoom} onClick={(e) => { handleRoomBtn(e) }}>Create Room</button>
             </div>
           </div>
         </div>
