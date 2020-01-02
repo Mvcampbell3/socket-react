ca0369d23903ab8383677cb3e10efb3cd835b0c1
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-02 17:57:47 -0500

    rebuilding testing area

--- client/src/testpages/Landing/index.js
+++ client/src/testpages/Landing/index.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import './Landing.css'
 
 const Landing = (props) => {
 
   const [newRoom, setNewRoom] = useState('');
-  const [goChat, setGoChat] = useState('');
+  const [goChat, setGoChat] = useState(false);
 
-  useEffect(() => {
-    // props.getRooms()
-  })
+  const testing = true;
 
   const handleNewRoomInput = (e) => {
     setNewRoom(e.target.value)
@@ -18,19 +17,43 @@ const Landing = (props) => {
   }
 
   return (
-    <div className="landing-container">
+    <div className="wrapper">
       <h3>This is the landing page</h3>
-      <button onClick={() => { props.getRooms() }}>Get Rooms</button>
 
-      <div className="form-container">
-        <div className="input-group">
-          <input type="text" placeholder='Room Name...' value={newRoom} onChange={(e) => { handleNewRoomInput(e) }} />
+      {testing ?
+        <div className='test-area'>
+          <h4 className='text-center'>Testing Area</h4>
+          <div className="testing-box">
+            <button onClick={() => { props.getRooms() }}>Get Rooms</button>
+          </div>
         </div>
-        <div className="input-group">
-          <button onClick={createRoom}>Create Room</button>
+        : null
+      }
+
+      <div className="landing-container">
+        <div className="left-side">
+          <div className="form-container">
+            <div className="input-group">
+              <input type="text" placeholder='Room Name...' value={newRoom} onChange={(e) => { handleNewRoomInput(e) }} />
+            </div>
+            <div className="input-group">
+              <button onClick={createRoom}>Create Room</button>
+            </div>
+          </div>
+        </div>
+        <div className="right-side">
+          {props.rooms.length > 0 ?
+            <>
+              {props.rooms.map(room => (
+                <div className="room">{room.name}</div>
+              ))}
+            </>
+            : <h4 className='text-center'>There are no rooms yet</h4>
+          }
         </div>
       </div>
-      
+
+
     </div>
   );
 }
