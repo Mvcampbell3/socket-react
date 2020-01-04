e47ec73bff77ff86974c8629b6ba3587059df714
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-04 15:09:49 -0500

    rebuilding component Landing

--- client/src/newApp.js
+++ client/src/newApp.js
@@ -2,51 +2,55 @@ import React, { Component } from 'react';
 
 import io from 'socket.io-client';
 
+import Header from './components/Header';
+import Landing from './components/Landing';
+import './App.css'
+
 class App extends Component {
 
   state = {
     socket: null,
     connected: false,
+    showLanding: true,
+    rooms: [],
     messages: []
   }
 
   componentDidMount() {
+    this.socketInit()
+  }
+
+  // Will contain ALL of the socket.on events
+  socketInit = () => {
     this.setState(prevState => {
       prevState.socket = io('localhost:3001');
       return prevState;
     }, () => {
       this.setState({ connected: true })
-      this.state.socket.emit('hello')
+
+      this.state.socket.emit('get rooms')
 
 
-      this.state.socket.on('hello back', (id) => {
-        console.log(id);
+      this.state.socket.on('rooms back', (rooms) => {
+        console.log(rooms);
         this.setState(prevState => {
-          prevState.messages = [...this.state.messages, id]
+          prevState.rooms = rooms.rooms;
           return prevState;
         })
       })
+
+
     })
-  }
-
-
-
-  emitHello = () => {
-    this.state.socket.emit('hello');
-  }
-
-  clearMessages = () => {
-    this.setState({messages: []})
   }
 
   render() {
     return (
-      <div className="container">
-        <button onClick={this.emitHello}>Hello</button>
-        <button onClick={this.clearMessages}>Clear</button>
-        {this.state.messages.map((message, i) => (
-          <p key={i}>{message}</p>
-        ))}
+      <div>
+        <Header />
+        {this.state.showLanding ? <Landing />
+        
+        :null}
+
       </div>
     );
   }
