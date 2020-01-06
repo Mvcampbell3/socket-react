24fced74907ee045adfedd0b9cfa63e3bd337993
Mvcampbell3 <mvcampbell3@gmail.com>
2020-01-06 16:18:34 -0500

    db into api routes

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ var app = express();
 var server = require('http').Server(app);
 const PORT = process.env.PORT || 3001;
 var io = require('socket.io')(server);
+const routes = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(routes)
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socketredo', { useUnifiedTopology: true, useNewUrlParser: true },
